78a54b404c492d63fba60d6a973b83758b55dc20
alejomj19971 <amuneraja@cesde.net>
2022-02-24 12:15:46 -0500

    3 primeros

---
+++ Ejercicio3.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.math.*;
+import java.util.Scanner;
+public class Ejercicio3{
+
+    public static void main(String[] args) {
+        Scanner inputData= new Scanner(System.in);
+        double square,cube,number;
+        System.out.println("Enter number : ");
+        number= inputData.nextDouble();
+        square=  Math.pow(number,2) ;
+        cube= Math.pow(number,3);
+        System.out.println("The number square is : "+square);
+        System.out.println("The number cube is   : "+cube);
+
+
+
+
+
+    }
+}
