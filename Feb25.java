d440bf6fa899de01d8279edaeeb9fa73244b4621
alejomj19971 <amuneraja@cesde.net>
2022-02-25 18:54:14 -0500

    par o no par

---
+++ Feb25.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+public class Feb25{
+
+    public static void main(String[] args) {
+        Scanner inputData= new Scanner(System.in);
+    int number;
+        System.out.println("Enter number:");
+    number=inputData.nextInt();
+    if (number%2==0) {
+        System.out.println("it is pair");
+
+    }
+
+    else{
+        System.out.println("it is not pair");
+    }
+
+
+
+    }
+}
