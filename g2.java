7e162691cc1869102423850019264ceb6e011cff
alejomj19971 <amuneraja@cesde.net>
2022-02-25 19:20:10 -0500

    segundo ejercicio

--- g2.java
+++ g2.java
@@ -1,2 +1,24 @@
-package com.company;public class g2 {
+package com.company;
+
+import java.util.Scanner;
+public class g2{
+
+    public static void main(String[] args) {
+        Scanner inputData= new Scanner(System.in);
+        int number;
+        System.out.println("Enter number:");
+        number=inputData.nextInt();
+
+        if (number%7 ==0) {
+            System.out.println("it is seven multiple");
+
+        }
+
+        else{
+            System.out.println("it is not seven multiple");
+        }
+
+
+
+    }
 }
