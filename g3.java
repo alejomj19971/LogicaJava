17131bf37af7e5d1389508b5e1b085c4b4c1caa3
alejomj19971 <amuneraja@cesde.net>
2022-02-25 19:56:19 -0500

    3 ejercicio

---
+++ g3.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+public class g3 {
+    public static void main(String[] args) {
+        Scanner inputData = new Scanner(System.in);
+        int numwheels, total;
+        System.out.println("Enter number of wheels : ");
+        numwheels = inputData.nextInt();
+        if (numwheels < 5) {
+            total = 30000 * numwheels;
+            System.out.println("The total amount for the wheels is :" + total);
+        }
+        else {
+
+            if (numwheels >= 5 && numwheels <= 10) {
+                total = 25000 * numwheels;
+                System.out.println("The total amount for the wheels is :" + total);
+
+
+            }
+            else{
+                if (numwheels > 10) {
+                    total = 20000 * numwheels;
+                    System.out.println("The total amount for the wheels is :" + total);
+
+
+                }
+            }
+
+
+            }
+
+
+    }
+}
